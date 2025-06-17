cce20eac1bc9190c3179b1fdee0a53e215e2f85c
Jayden <jayden.heberhendrickse@gmail.com>
2025-06-17 17:23:17 +0200

    created to test cases

--- KnightTest.js
+++ KnightTest.js
@@ -1,4 +1,8 @@
 // Runs some test cases for the Knight Travails challenge
 import { KnightMoves } from './KnightTravails'
 
-console.log("Test 1:\nstart: [0, 0]\nend: ")
+console.log("Test 1:\nstart: [0, 0]\nend: [3, 3]");
+KnightMoves([0,0],[3,3]);
+
+console.log("Test 2:\nstart: [3, 3]\nend: [0, 0]");
+KnightMoves([3,3],[0,0],true);
